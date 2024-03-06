fff6561f4290074225e6b29be0c01eb3d9bde299
Elliott Shim <shime@oregonstate.edu>
2024-03-06 11:42:26 -0800

    added test that verifies that loadAllSavedCharts loads all charts correctly. Also added some code after every test that clears the localStorage so that each test is isolated.

--- src/lib/chartStorage.test.js
+++ src/lib/chartStorage.test.js
@@ -24,6 +24,8 @@ test ('whether charts are saved with saveChart', function() {
     // Assert
     expect(JSON.parse(result)).not.toEqual([])
 
+    // Clears the chart storage
+    window.localStorage.clear()
 })
 
 test ('whether correct chart is saved with saveChart', function() {
@@ -48,6 +50,57 @@ test ('whether correct chart is saved with saveChart', function() {
         chart
     )
 
+    // Clears the chart storage
+    window.localStorage.clear()
 })
 
+test ('whether all saved charts are found with loadAllSavedCharts', function() {
+    // Arrange
+    const chart1 = {
+        type: 'bar',
+        data: {
+            labels: ['F','D','C','B','A'],
+            datasets: [{
+                label: 'Students',
+                data: [10,20,30,40,30]
+            }]
+        }
+    }
 
+    const chart2 = {
+        type: 'line',
+        data: {
+            labels: ['F','D','C','B','A'],
+            datasets: [{
+                label: 'Students',
+                data: [10,20,30,40,30]
+            }]
+        }
+    }
+
+    const chart3 = {
+        type: 'scatter',
+        data: {
+            labels: ['F','D','C','B','A'],
+            datasets: [{
+                label: 'Students',
+                data: [10,20,30,40,30]
+            }]
+        }
+    }
+
+    // Act
+    chartStorage.saveChart(chart1)
+    chartStorage.saveChart(chart2)
+    chartStorage.saveChart(chart3)
+    result = chartStorage.loadAllSavedCharts()
+
+    // Assert
+    expect(result).toHaveLength(3)
+    expect(result[0]).toStrictEqual(chart1)
+    expect(result[1]).toStrictEqual(chart2)
+    expect(result[2]).toStrictEqual(chart3)
+
+    // Clears the chart storage
+    window.localStorage.clear()
+})
