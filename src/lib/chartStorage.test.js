e77462f269e1cf815c1a8d23a03c58d0a2c00f6d
Elliott Shim <shime@oregonstate.edu>
2024-03-06 10:53:47 -0800

    added test that verifies that charts are saved correctly

--- src/lib/chartStorage.test.js
+++ src/lib/chartStorage.test.js
@@ -6,9 +6,24 @@ const chartStorage = require("./chartStorage")
 
 test ('whether charts are saved with saveChart', function() {
     // Arrange
+    const chart = {
+        type: 'bar',
+        data: {
+            labels: ['F','D','C','B','A'],
+            datasets: [{
+                label: 'Students',
+                data: [10,20,30,40,30]
+            }]
+        }
+    }
 
     // Act
+    chartStorage.saveChart(chart)
+    result = window.localStorage.getItem("savedCharts")
 
     // Assert
+    expect(JSON.parse(result)[0]).toStrictEqual(
+        chart
+    )
 
 })
