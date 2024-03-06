6e0349f204fe707783502c4f9d60f2e85f6bbdb0
Elliott Shim <shime@oregonstate.edu>
2024-03-06 11:10:32 -0800

    added test that verifies that charts are saved with saveChart. Different from previous in that it just checks that an empty list is not returned.

--- src/lib/chartStorage.test.js
+++ src/lib/chartStorage.test.js
@@ -22,8 +22,32 @@ test ('whether charts are saved with saveChart', function() {
     result = window.localStorage.getItem("savedCharts")
 
     // Assert
+    expect(JSON.parse(result)).not.toEqual([])
+
+})
+
+test ('whether correct chart is saved with saveChart', function() {
+    // Arrange
+    const chart = {
+        type: 'bar',
+        data: {
+            labels: ['F','D','C','B','A'],
+            datasets: [{
+                label: 'Students',
+                data: [10,20,30,40,30]
+            }]
+        }
+    }
+
+    // Act
+    chartStorage.saveChart(chart)
+    result = window.localStorage.getItem("savedCharts")
+
+    // Assert
     expect(JSON.parse(result)[0]).toStrictEqual(
         chart
     )
 
 })
+
+
