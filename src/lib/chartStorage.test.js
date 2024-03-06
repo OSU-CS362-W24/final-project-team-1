d9a3ed878ef5b80dc42e8333029e626ad0c47877
Elliott Shim <shime@oregonstate.edu>
2024-03-06 10:53:02 -0800

    added chartStorage.test.js to begin testing. Also added an empty starter test template.

---
+++ src/lib/chartStorage.test.js
@@ -0,0 +1,14 @@
+/**
+* @jest-environment jsdom
+*/
+
+const chartStorage = require("./chartStorage")
+
+test ('whether charts are saved with saveChart', function() {
+    // Arrange
+
+    // Act
+
+    // Assert
+
+})
