8444defdaeda1ab783c168c66a7b8ace7a4fa28d
dualsm <rynersos@oregonstate.edu>
2024-03-11 14:27:51 -0700

    added test file

---
+++ src/line/line.test.js
@@ -0,0 +1,18 @@
+/**
+* @jest-environment jsdom
+*/
+
+const fs = require("fs")
+const domTesting = require('@testing-library/dom')
+require('@testing-library/jest-dom')
+const userEvent = require("@testing-library/user-event").default
+
+function initDomFromFiles(htmlPath, jsPath) {
+	const html = fs.readFileSync(htmlPath, 'utf8')
+	document.open()
+	document.write(html)
+	document.close()
+	jest.isolateModules(function() {
+		require(jsPath)
+	})
+}
